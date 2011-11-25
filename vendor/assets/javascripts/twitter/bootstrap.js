084a2d228a5ae584b258c00eb80d98228d785dc6
Sébastien Gruhier <sebastien.gruhier@xilinus.com>
2011-11-25 09:54:50 +0100

    force JS order

--- vendor/assets/javascripts/twitter/bootstrap.js
+++ vendor/assets/javascripts/twitter/bootstrap.js
@@ -1,1 +1,10 @@
-//= require_tree ./bootstrap
+//= require twitter/bootstrap/bootstrap-accordion.js
+//= require twitter/bootstrap/bootstrap-alerts.js
+//= require twitter/bootstrap/bootstrap-buttons.js
+//= require twitter/bootstrap/bootstrap-dropdown.js
+//= require twitter/bootstrap/bootstrap-twipsy.js
+//= require twitter/bootstrap/bootstrap-modal.js
+//= require twitter/bootstrap/bootstrap-popover.js
+//= require twitter/bootstrap/bootstrap-scrollspy.js
+//= require twitter/bootstrap/bootstrap-tabs.js
+//= require twitter/bootstrap/bootstrap-transitions.js
